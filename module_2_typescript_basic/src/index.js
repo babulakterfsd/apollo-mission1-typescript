b7b83cf9a4ae63aee42d476234a34a49e44fba0d
Babul Akter <babulakterfsd@gmail.com>
2023-11-02 20:33:43 +0600

    tupple wrote

---
+++ module_2_typescript_basic/src/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const tuppleAray = [1, true, 'abc', false]; // tupple hocche emon ekta array jekhane data serial aage theke bole deya hoy oi array element er. oi serial na manle error dibe typescript
+console.log(tuppleAray);
