65246abc220e1605cb1b1471c0b29eab60df495a
Babul Akter <babulakterfsd@gmail.com>
2023-11-04 10:52:52 +0600

    module 1 completed

--- module_2_typescript_basic/src/index.ts
+++ module_2_typescript_basic/src/index.ts
@@ -1,3 +1,3 @@
-const tuppleAray : [number, boolean, string, boolean] = [1, true, 'abc', false]; // tupple hocche emon ekta array jekhane data serial aage theke bole deya hoy oi array element er. oi serial na manle error dibe typescript
+const tuppleAray : [number, boolean, string, boolean] = [1, true, 'abcd', false]; // tupple hocche emon ekta array jekhane data serial aage theke bole deya hoy oi array element er. oi serial na manle error dibe typescript
 
 console.log(tuppleAray);
